02d96f646cf49cb886f9391a4b16f9bfd3abd57c
Julián España <julii1295@gmail.com>
2022-09-10 22:06:13 -0500

    Siguiendo la construcción del juego de adivinanzas

--- 12_gameAdivina/game.js
+++ 12_gameAdivina/game.js
@@ -6,3 +6,16 @@
 // console.log(`La palabra por adivinar es ${word}`);
 
 console.log('registrar las veces que doy clic sobre el boton');
+
+// arreglo de palabras para adivinar 
+var words = ['casa','mueble','televisor']
+
+// trayendo etiquetas de html
+var letra = document.querySelector('input') 
+
+function adivinar(params) {
+
+    if (letra == words.values) {
+        alert('Lo lograste')
+    }
+}
